a85f978b0d5476adb707029d08eeee555b622193
Rogier Wijsman <rogierwijsman@gmail.com>
2015-03-23 00:10:04 +0100

    comitten

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some 
+## benefit to caching the inverse of a matrix rather than computing it 
+## repeatedly. The assignment is to write a pair of functions that 
+## cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(data_matrix = matrix()) 
+{
+    
+    stored_inverse   <- NULL
+    
+    set <- function(y) 
+    {
+      data_matrix <<- y
+      stored_inverse <<- NULL
+    }
+    
+    get <- function() 
+    {
+      return (data_matrix)
+    }
+    
+    setinverse <- function(sent_replacement_inverse)
+    {
+      stored_inverse <<- sent_replacement_inverse
+    }
+    
+    getinverse <- function() 
+    {
+      return(stored_inverse)
+    }
+    
+    
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special 
+## "matrix" returned by makeCacheMatrix above. If the 
+## inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(madeMatrix, ...) 
+{
         ## Return a matrix that is the inverse of 'x'
+      
+    local_inverse <- madeMatrix$getinverse()
+    
+    if(!is.null(local_inverse)) 
+    {
+      message("getting cached data")
+      return(local_inverse)
+    }
+    else 
+    {
+      local_data <- madeMatrix$get()
+      local_inverse <- solve(local_data, ...)
+      
+      madeMatrix$setinverse(local_inverse)
+      return(local_inverse) 
+    }
 }
